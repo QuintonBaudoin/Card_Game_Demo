6994182ed2a47b43d7a4c4edaaef3bf7e99f5737
Quinton Baudoin <baudoin.q@gmail.com>
2016-04-14 14:21:53 -0500

    Added FieldInfo and GatherFieldInfo()
    Read the Comments for more descriptions

--- Assets/Scripts/FieldHandler.cs
+++ Assets/Scripts/FieldHandler.cs
@@ -2,11 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Events;
+using System;
 
 namespace Quinton
 {
     /// <summary>
-    /// Information of Field Event
+    /// Information of Resolution:
+    /// 
+    /// Messages:
+    /// 
+    /// GoodDudesWin -> If GoodDudes have higher power
+    /// BadDudesWin  -> If BadDudes have higher power or If both have same power
+
+    /// 
     /// </summary>
     public class ResolutionInfo
     {
@@ -16,12 +24,35 @@ namespace Quinton
         public ResolutionInfo(string msg)
         {
             Message = msg;
-            
+
         }
-            
+
     }
 
 
+    public class FieldInfo
+    {
+        public List<GameObject> GoodDudes;
+        public List<GameObject> BadDudes;
+
+        string LastMessage;
+
+
+        public FieldInfo(ref List<GameObject> gdudes,ref List<GameObject> bdudes, string msg)
+        {
+            GoodDudes = gdudes;
+            BadDudes = bdudes;
+
+            LastMessage = msg;
+
+        }
+
+    }
+
+
+    /// <summary>
+    /// Field Event. Structured as Singleton
+    /// </summary>
     public class FieldEvent : UnityEvent<ResolutionInfo>
     {
 
@@ -37,17 +68,39 @@ namespace Quinton
                 return _instance;
             }
         }
-        
+
 
         private FieldEvent()
         { }
 
     }
 
-   public class FieldHandler : MonoBehaviour
+    /// <summary>
+    /// Handles "Combat" between GoodDudes and BadDudes 
+    /// (Both are List<GameObject>
+    /// </summary>
+    /// <body>
+    /// When Resolve() method is called the lists of GoodDudes 
+    /// and BadDudes will be compared
+    /// The group with the highest "Power" will "win".
+    /// If resalting in tie the BadDudes will "win"
+    /// 
+    /// To add to a list use the Add----Dudes to pass in a list
+    /// and Add----Dude to pass in a single GameObjects.
+    /// 
+    /// Note: Will only compare those that are of the 
+    /// MysteryCardMono type.
+    /// 
+    /// To collect information of the field use GatherFieldInfo() 
+    /// this will return a FieldInfo object that contains
+    /// the GoodDudes and BadDudes. As well ass the last message 
+    /// sent threw the event system
+    /// </body> 
+    public class FieldHandler : MonoBehaviour
     {
-        static private FieldHandler _instance;
 
+        static private FieldHandler _instance;
+             
         public static FieldHandler instance
         {
             get
@@ -58,6 +111,8 @@ namespace Quinton
                 return _instance;
             }
         }
+
+        string m_LastMessage = "No Resolutions Made";
 
         public FieldEvent fieldEvent = FieldEvent.instance;
 
@@ -91,9 +146,14 @@ namespace Quinton
         }
         public void Resolve()
         {
-            int GoodDudesPower=0;
+            int GoodDudesPower = 0;
             int BadDudesPower = 0;
+            string resolveMessage;
 
+
+            if (GoodDudes.Count <= 0 || BadDudes.Count <= 0)
+                throw new Exception("Missing Cards: " + "GoodDudesCount: " + GoodDudes.Count + " BadDudesCount: " + BadDudes.Count);
+               
 
             foreach (GameObject go in GoodDudes)
                 if (go.GetComponent<MysteryCardMono>() != null)
@@ -104,15 +164,20 @@ namespace Quinton
 
 
 
-            string resolveMessage;
+            
 
             if (GoodDudesPower > BadDudesPower)
                 resolveMessage = "GoodDudesWin";
-            else if (GoodDudesPower < BadDudesPower)
+            else
                 resolveMessage = "BadDudesWin";
-            else resolveMessage = "Tie";
+            m_LastMessage = resolveMessage;
             fieldEvent.Invoke(new ResolutionInfo(resolveMessage));
-            
+
+        }
+
+        public FieldInfo GatherFieldInfo()
+        {
+            return new FieldInfo(ref GoodDudes, ref BadDudes, m_LastMessage);
         }
 
     }
